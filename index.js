24d059a79f72eebbcd08135774ee89aea16e2352
Caitlyn <meszaros.caitlyn@gmail.com>
2021-06-08 17:52:17 -0400

    commit tree 7b0ee9266f68a13297ad9e62852832fbd2f3e63b

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+// Test 1
+const num1 = 31
+const num2 = 2
 
-// Write your code here
+const multiply = num1*num2
+
+// Test 2
+const random = Math.floor(Math.random()*10)
+
+// Test 3
+const num3 = 40052
+const num4 = 16
+const mod = num3 % num4; //Why do I have to declare? When do I don't?
+
+// Test 4
+const max = Math.max(1,4,8,20);
